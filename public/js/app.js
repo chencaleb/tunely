f7dc50ac194017ca3cbcf5c58dba5e77d253dcc7
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-03-07 09:10:02 -0800

    Revert "Adds commit before merging css2"
    This reverts commit d3d2fa38f0d0e7090b943c11de6602928cfc5c05, reversing
    changes made to 226f966a1be65d91282da8260ba3ea91f646886e.

--- public/js/app.js
+++ public/js/app.js
@@ -33,20 +33,7 @@ sampleAlbums.push({
 /* end of hard-coded data */
 
 $(document).ready(function() {
-
-  //splash page fadein
   console.log('app.js loaded!');
-
-  if($(".splash").is(":visible"))
-  {
-    $(".wrapper").css({"opacity":"0"});
-  }
-  $("#vinyl").click(function()
-  {
-    $(".splash").slideUp("800", function() {
-        $(".wrapper").delay(200).animate({"opacity":"1.0"});
-     });
-  });
 
 // AJAX album methods
   //get method
@@ -346,13 +333,5 @@ function renderGenre(genre){
 
   $("#genres").append(genreHtml);
 }
-
-$(window).scroll(function() {
-      $(window).off("scroll");
-    $(".splash").slideUp("800", function() {
-    $("html, body").animate({"scrollTop":"0px"},100);
-    $(".wrapper").delay(100).animate({"opacity":"1.0"},800);
- });
- });
 
 
